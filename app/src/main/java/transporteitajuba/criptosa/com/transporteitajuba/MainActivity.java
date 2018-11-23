0aa01de1718fd6193c307c3b4be63b471ec9cc90
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2018-11-23 00:25:21 -0200

    Listeners Menu
    Listeners Menu

--- app/src/main/java/transporteitajuba/criptosa/com/transporteitajuba/MainActivity.java
+++ app/src/main/java/transporteitajuba/criptosa/com/transporteitajuba/MainActivity.java
@@ -1,16 +1,55 @@
 package transporteitajuba.criptosa.com.transporteitajuba;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    //Imagens (botões) do Menu Principal
+    private ImageView btnHorarios;
+    private ImageView btnParadas;
+    private ImageView btnMapas;
+    private ImageView btnRota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Capturando botões do menu
+        btnHorarios = (ImageView) findViewById(R.id.imgHorario);
+        btnParadas = (ImageView) findViewById(R.id.imgParadas);
+        btnMapas = (ImageView) findViewById(R.id.imgMapas);
+        btnRota = (ImageView) findViewById(R.id.imgRotas);
 
+        //Listeners botões Menu
+        btnHorarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SelecaoHorario.class));
+            }
+        });
+        btnMapas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this, aEmpresa.class));
+            }
+        });
+        btnParadas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this, aEmpresa.class));
+            }
+        });
+        btnRota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this, aEmpresa.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/transporteitajuba/criptosa/com/transporteitajuba/SelecaoHorario.java
@@ -0,0 +1,14 @@
+package transporteitajuba.criptosa.com.transporteitajuba;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelecaoHorario extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_horario);
+    }
+}
